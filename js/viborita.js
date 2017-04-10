cd84f92fdda29e44f6d54237f54ae134d57b6bc1
Rocio Avila <Rocio Avila>
2017-04-10 09:16:50 -0500

    Inicio sin funcionar

---
+++ js/viborita.js
@@ -0,0 +1,18 @@
+//alert("hola");
+
+var jaula = document.getElementById("jaula");
+var cabezaVibora = document.getElementById("cabezaVibora");
+var posicionInicialY = 250;
+var posicionInicialX = 250;
+var avanzarX = 20;
+var avanzarY = 20;
+var teclas = {
+        UP:38,  // usando el codigo ASCII del teclado
+        DOWN:40,
+        LEFT:37,
+        RIGHT:39
+}; 
+
+function avanzar(){
+
+}
